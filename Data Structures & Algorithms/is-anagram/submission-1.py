b088def5f5194d4fa6dc0975327d80f492718615
Nohim Jayasinghe <nvj@sfu.ca>
2026-08-24 20:54:01 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        freq1 = {}
+        freq2 = {}
+        for char in s:
+            freq1[char] =  freq1.get(char, 0) + 1
+        for char in t:
+            freq2[char] = freq2.get(char, 0) + 1
+        if freq1 == freq2:
+            return True
+        else:
+            return False
+        
